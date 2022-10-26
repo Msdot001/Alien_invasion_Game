a47fe6b261d74883b22a2042728ded49928f0017
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-10-26 17:16:29 +0200

    Adjusting the Ship’s Speed

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,4 +7,5 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (230, 230, 230)           # control screen color 
+        self.ship_speed = 1.5 
