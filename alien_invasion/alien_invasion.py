3b0d58082f56fcfc2e53774ccb168ff3afb06078
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-10-26 14:41:13 +0200

    Adding Responding to a Keypress

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -35,7 +35,7 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            self._check_event()
+            self._check_events()
             self._update_screen()
 
     def _check_events(self):
@@ -43,6 +43,11 @@ class AlienInvasion:
         for event in pygame.event.get():       
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
        """Updqate images on the screen, and flip to the new screen.""" 
