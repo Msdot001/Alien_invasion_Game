3bdce2213d8b57a7656dd2eee8893984b16556c2
Moshood Owolabi <98819100+Msdot001@users.noreply.github.com>
2022-11-01 16:19:20 +0100

    Creating and Storing bullets in a Group

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,40 +3,41 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+"""
+The object we assigned to self.screen is called a surface.
+A surface in Pygame is a part of the screen where a game element can be displayed
+In this case, Game element is an alien or a ship,
+
+- Refactoring simplifies the structure of the code youve already written, making it easier to build on.
+- A helper method does work inside a class but isnt meant to be called through an instance. 
+-  In Python, a single leading underscore indicates a helper method.
+
+The run_game() is splitted into two helper method (_check_event() and _update_screen())
+
+"""
 
 
 class AlienInvasion:
     """Overall class to manage game assest and behavior"""
 
     def __init__(self):
-        """Initialize the game, and create game resourrce"""
+        """Initialize the game, and create game resource"""
         pygame.init()
         self.settings = Settings()
-        """
-        The object we assigned to self.screen is called a surface.
-        A surface in Pygame is a part of the screen where a game element can be displayed
-        In this case, Game element is an alien or a ship,
-        """
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))              # setting background screen with a dimension of 1200 * 800         
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))              # setting background screen with a dimension of 1200 * 800    
         pygame.display.set_caption("Alien Invasion By Moshood")
-
+        
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
-    """
-      - Refactoring simplifies the structure of the code youve already written, making it easier to build on.
-      - A helper method does work inside a class but isnt meant to be called through an instance. 
-      -  In Python, a single leading underscore indicates a helper method.
-
-      The run_game() is splitted into two helper method (_check_event() and _update_screen())
-
-    """  
 
     def run_game(self):             # run_game was refractor into _check_events() and _update_screen() method                                     
         """Start the main loop for the game"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()            
             self._update_screen()
 
     def _check_events(self):
@@ -67,7 +68,7 @@ class AlienInvasion:
 
 
     def _update_screen(self):
-       """Updqate images on the screen, and flip to the new screen.""" 
+       """Update images on the screen, and flip to the new screen.""" 
        self.screen.fill(self.settings.bg_color)
        self.ship.blitme()
        # Make the most recently drawn screen visible
